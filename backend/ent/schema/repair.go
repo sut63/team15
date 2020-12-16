39dc14bcefcad5358068d40d7109de1ae7c7601d
prakitsrakaew <prakitsra@gmail.com.com>
2020-12-16 13:40:13 +0700

    do entity for repair system -close #

---
+++ backend/ent/schema/repair.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Repair holds the schema definition for the Repair entity.
+type Repair struct {
+	ent.Schema
+}
+
+// Fields of the Repair.
+func (Repair) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Repair.
+func (Repair) Edges() []ent.Edge {
+	return nil
+}
