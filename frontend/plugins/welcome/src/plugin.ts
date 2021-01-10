6b77bf26ad03c2854dc0463257e78de094577812
save-a-gain <69420912+save-a-gain@users.noreply.github.com>
2021-01-10 23:47:20 +0700

    ทำ sign in กับ frontend ของระบบรับฝากพัสดุ - close #54

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,14 +1,16 @@
 import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
-import WatchVideo from './components/WatchVideo'
-import SignIn from './components/SignIn'
+import CreateDeposit from './components/RecordDeposit';
+import ShowDeposit from './components/DepositTable';
+import SignIn from './components/SignIn';
 
 
 export const plugin = createPlugin({
   id: 'welcome',
   register({ router }) {
-    router.registerRoute('/', WelcomePage);
-    router.registerRoute('/watch_video', WatchVideo);
-    router.registerRoute('/signin', SignIn);
+    router.registerRoute('/', SignIn);
+	router.registerRoute('/Home', WelcomePage);
+    router.registerRoute('/DepositTable', ShowDeposit);
+    router.registerRoute('/RecordDeposit', CreateDeposit);
   },
 });
